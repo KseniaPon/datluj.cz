967363ef6174cb8877a852f288a527b52012e2d1
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-05-13 18:55:17 +0200

    datluj 1. fáze

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const Wordbox = ({ word }) => {
   const [lettersLeft, setLettersLeft] = useState(word);  
+
+  const handleKeyUp = (e) => {
+    console.log(e)
+    if (e.key === lettersLeft[0]) {
+      setLettersLeft(lettersLeft.substr(1))
+      console.log(lettersLeft.substr(1))
+    }
+    
+  }
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyUp)
+
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [lettersLeft])
+
+  
   
   return (
     <div className="wordbox">{lettersLeft}</div>
